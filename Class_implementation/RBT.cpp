2410c369dc5c7fd8d58fece9f0792e07e03896c1
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-04-10 16:25:51 +0800

    feat: Add Red-Black Tree

---
+++ Class_implementation/RBT.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: Xiyuan Yang   xiyuan_yang@outlook.com
+ * @Date: 2025-04-10 15:01:57
+ * @LastEditors: Xiyuan Yang   xiyuan_yang@outlook.com
+ * @LastEditTime: 2025-04-10 16:25:26
+ * @FilePath: /Data_structure/Class_implementation/RBT.cpp
+ * @Description: 
+ * Do you code and make progress today?
+ * Copyright (c) 2025 by Xiyuan Yang, All Rights Reserved. 
+ */
+#include <cstdio>
+
+template<class Key, class Other>
+class RBT {
+};
